b6cffe02fede944423862686182d30285439adda
Savita <96195601+savigurjar@users.noreply.github.com>
2024-07-29 13:58:41 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // size of array
+    int arr[7] = {12, 13, 10, 7, 30, 95, 28};
+    cout << sizeof(arr) << endl; // each element contain 4 byts
+
+    // how many element in array arr[0]= 4byts
+    int arrr[] = {12, 13, 10, 7, 30, 95, 28};
+    cout << "elemnts in array " << sizeof(arrr) / sizeof(arrr[0]) << endl;
+
+    // sizeof is in-built function
+}
